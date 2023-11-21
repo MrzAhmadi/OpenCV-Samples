f8d8e9ec2442684048f1eb1f302063a8ed566d0e
Mohammadreza <smrahmadi12@gmail.com>
2023-11-21 19:26:54 +0100

    Reformat the whole of project

--- 01- Image/show_image.py
+++ 01- Image/show_image.py
@@ -1,15 +1,15 @@
 import cv2 as cv
 import sys
 
-#find the image
+# find the image
 img = cv.imread(cv.samples.findFile("camaro.jpg"))
-#check it isn't null
+# check it isn't null
 if img is None:
     sys.exit("Could not read the image.")
-#show Image
+# show Image
 cv.imshow("Camaro zl1 2024", img)
-#hold image until user press a key
+# hold image until user press a key
 k = cv.waitKey(0)
-#if the pressed key was 'S' write image as new file
+# if the pressed key was 'S' write image as new file
 if k == ord("s"):
     cv.imwrite("new_camaro.jpg", img)
